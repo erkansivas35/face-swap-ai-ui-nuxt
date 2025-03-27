50a5991135759e2f87f9e0988f8e02b233000479
erkansivas35 <erkan.sivas35@gmail.com>
2025-03-27 14:14:53 +0300

    add Tailwind CSS module and configuration to Nuxt setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+  },
   app: {
     head: {
       title: 'Free Face Swap AI - Online (No Signup)',
