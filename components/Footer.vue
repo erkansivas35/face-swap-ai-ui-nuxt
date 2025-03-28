20b397bf4a45b0a1bae706eed409c72555edfe7c
erkansivas35 <erkan.sivas35@gmail.com>
2025-03-28 18:10:04 +0300

    feat: implement new footer component with navigation links and copyright notice

---
+++ components/Footer.vue
@@ -0,0 +1,32 @@
+<script setup>
+const items = [
+  [
+    {
+      label: 'Face Swap Photo',
+      to: '/',
+    },
+  ],
+  [
+    {
+      label: 'Easy AI Tools All Copyright Reserved @ 2025',
+      class: 'hover:text-neutral hover:bg-slate-900'
+    },
+    {
+      label: '|',
+      class: 'hover:text-neutral hover:bg-slate-900'
+    },
+    {
+      label: 'Contact',
+      to: 'mailto:erkan@erkansivas.xyz'
+    },
+  ],
+]
+</script>
+
+<template>
+  <footer class="w-full max-w-5xl mx-auto text-sm py-8 px-4 md:px-4 lg:px-0">
+    <div class="pt-5 mt-5 border-t border-neutral-700">
+      <UNavigationMenu color="neutral" :items="items" variant="link" class="w-full" />
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,3 @@
-<script setup>
-const items = [
-  [
-    {
-      label: 'Face Swap Photo',
-      to: '/',
-    },
-  ],
-  [
-    {
-      label: 'Easy AI Tools All Copyright Reserved @ 2025',
-      class: 'hover:text-neutral'
-    },
-    {
-      label: '|',
-      class: 'hover:text-neutral'
-    },
-    {
-      label: 'Contact',
-      to: 'mailto:erkan@erkansivas.xyz'
-    },
-  ],
-]
-</script>
-
 <template>
   <Header />
   <main class="w-full">
@@ -30,10 +5,5 @@ const items = [
         <slot />
       </div>
   </main>
-
-  <footer class="w-full max-w-5xl mx-auto text-sm py-8 px-4 md:px-4 lg:px-0">
-    <div class="pt-5 mt-5 border-t border-neutral-700">
-      <UNavigationMenu color="neutral" :items="items" class="w-full" />
-    </div>
-  </footer>
+  <Footer />
 </template>
