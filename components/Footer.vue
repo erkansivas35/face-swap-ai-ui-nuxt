c4f434dd8007ca648054e0cbbf57d95828e5b3ca
erkansivas35 <erkan.sivas35@gmail.com>
2025-04-03 19:25:07 +0300

    feat: add Terms of Service and Privacy Policy links to registration form and remove sitemap.xml

--- components/Footer.vue
+++ components/Footer.vue
@@ -27,6 +27,22 @@
         <ul class="flex flex-wrap items-center">
           <li
             class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
+            <NuxtLink
+              class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-hidden focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400"
+              to="/privacy-policy">
+              Privacy Policy
+            </NuxtLink>
+          </li>
+          <li
+            class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
+            <NuxtLink
+              class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-hidden focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400"
+              to="/terms-of-service">
+              Terms of Service
+            </NuxtLink>
+          </li>
+          <li
+            class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
             <a class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-hidden focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400"
               href="mailto:erkan@erkansivas.xyz">
               Contact
